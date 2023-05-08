447d021fc5250cb3c274010b4c4a9c826e9e9006
Swathi <swathika.sidharth@gmail.com>
2023-05-08 15:51:53 -0400

    Adding Files

---
+++ Printing Practice/src/HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) {
+    String output = "Hi " + "Swathika"+ " Ravindran!";
+    System.out.println(output);
+    }
+}
---
+++ Printing Practice/src/MyNameIs.java
@@ -0,0 +1,8 @@
+public class MyNameIs {
+    public static void main(String[] args){
+        System.out.println("My name is");
+        System.out.println("Swathika");
+        System.out.println("Ravindran");
+
+    }
+}
