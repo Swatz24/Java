b5892c698cc8e88d2435033571bf4f946cd3b904
Swathi <swathika.sidharth@gmail.com>
2023-05-09 11:45:51 -0400

    Adding Files to Calculations folder

---
+++ Calculations/src/Calculations.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Calculations {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a = input.nextInt();
+        System.out.println("Enter another number");
+        int b = input.nextInt();
+        System.out.println(a + " + "+ b + " = " + (a+b));
+        System.out.println(a + " - "+ b + " = " + (a-b));
+        System.out.println(a + " * "+ b + " = " + (a*b));
+        System.out.println(a + " / "+ b + " = " + ((float)a/(float)b));
+        System.out.println(a + " % "+ b + " = " + (a%b));
+        }
+    }
