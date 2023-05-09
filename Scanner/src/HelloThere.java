4c2d29c01eb74184ed28bf0ed3fa365c2415e4b5
Swathi <swathika.sidharth@gmail.com>
2023-05-09 10:45:41 -0400

    Adding Files to Scanner folder

--- Scanner/src/HelloThere.java
+++ Scanner/src/HelloThere.java
@@ -3,9 +3,11 @@ public class HelloThere {
     public static void main(String[] args){
         System.out.println("Hello there! How are you doing today?");
         Scanner input = new Scanner(System.in);
-        String response = input.next();
+        String response = input.nextLine();
+        System.out.println(response);
         System.out.println("Wow - Interesting! Tell me more!");
-        String response2 = input.next();
+        String response2 = input.nextLine();
+        System.out.println(response2);
         System.out.println("Nice chatting with you! GoodBye!");
     }
 }
