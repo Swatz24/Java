70ffe871d95dbe0c3d87e4a6c9cbf2b4695576a8
Swathi <swathika.sidharth@gmail.com>
2023-05-23 12:11:38 -0400

    Debug assignment

---
+++ Debug/src/Main.java
@@ -0,0 +1,79 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        // Test your method with an array containing the numbers 1, 2, 3, 4, 5. Your method should return 3.0.
+        int[] numbers = {1,2,3,4,5};
+
+        Average avg = new Average();
+
+        double average = avg.calculateAverage(numbers);
+        System.out.println("Average of array of integers: " + average);
+
+        try{
+            // Null pointer exception here because the array value is not passed or assigned through constructor, but we are trying to print the array using print() function.
+            // which will throw a NullPointerException. using try, catch block to catch the exception when it occurs.
+            avg.print();
+        }catch (NullPointerException e){
+            System.out.println(e.getMessage());
+        }
+
+        // Index out of bound exception because we are trying to print numbers array with the index 7,
+        // since the array length is 5, we get this exception here
+        try {
+            System.out.println(numbers[7]);
+        } catch (IndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+
+        // Logic error
+
+        System.out.println("Average of numbers in the array: (Logic error: number[0] is excluded while calculating sum )");
+        System.out.println(avg.erroredAverage(numbers));
+    }
+
+    public static class Average{
+        private int[] numbers;
+
+        public Average() {
+
+        }
+
+        //  calculates the average of an array of integers.
+        //  This method should take an array of integers as input and return a double as output.
+        public double calculateAverage(int[] arr){
+            int sum = 0;
+            for(int i = 0; i < arr.length; i++){
+                if(i >= arr.length ){
+                    throw new ArrayIndexOutOfBoundsException("Array out of bound exception occurred.");
+                }
+                sum+= arr[i];
+            }
+            return (double) sum /arr.length;
+
+        }
+
+        public void print(){
+            for (int number : numbers) {
+                if (numbers.length == 0) {
+                    throw new NullPointerException("Array is empty");
+                }
+                System.out.println(number + "\n");
+            }
+        }
+
+        public double erroredAverage(int[] arr){
+            double sum=0;
+            // Logic error here, while calculating sum, we have to start from arr index 0 , but here it we start from array index 1. we can find the problem
+            // by printing the arr[i] inside the loop to see what we are missing.
+
+            for(int i=1; i< arr.length; i++){
+                System.out.println(arr[i]); // Now you will see that arr[0] is missed while calculating sum.
+                sum+=arr[i];
+            }
+            return  sum/arr.length;
+        }
+
+    }
+}
